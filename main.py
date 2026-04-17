91d47f9a33f506b7cd9a8169e6383d1d8f71b983
kanchu2362-ux <kanchu2362@gmail.com>
2026-04-17 07:41:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import streamlit as st
 
+# Import methods
 from methods.gauss_seidel import gauss_seidel_app
-from methods.trapezoidal_rule import trapezoidal_app
 from methods.milne_method import milne_app
+from methods.trapezoidal_rule import trapezoidal_app
 
 
 st.set_page_config(
-    page_title="Numerical Methods Calculator",
-    page_icon="📐",
-    layout="wide"
+    page_title="Numerical Methods Solver",
+    layout="centered"
 )
 
-st.title("📐 Numerical Methods Smart Calculator")
+st.title("Numerical Methods Calculator")
 
-st.sidebar.title("Navigation")
-
-page = st.sidebar.radio(
-    "Select Method",
-    [
-        "Gauss Seidel Method",
-        "Trapezoidal Rule",
-        "Milne Predictor Corrector"
-    ]
+method = st.sidebar.selectbox(
+    "Choose Method",
+    (
+        "Gauss-Seidel",
+        "Milne Method",
+        "Trapezoidal Rule"
+    )
 )
 
-if page == "Gauss Seidel Method":
+if method == "Gauss-Seidel":
     gauss_seidel_app()
 
-elif page == "Trapezoidal Rule":
-    trapezoidal_app()
-
-elif page == "Milne Predictor Corrector":
+elif method == "Milne Method":
     milne_app()
+
+elif method == "Trapezoidal Rule":
+    trapezoidal_app()
